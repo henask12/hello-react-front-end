1ded1aa6b89ac1c08d0b0794251a2bd23df39b10
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-01 22:54:43 +0300

    Greetings Slice

---
+++ src/redux/greetings/greetingsSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchGreetings = createAsyncThunk(
+  'greetings/fetchGreetings',
+  async (thunkApi) => {
+    try {
+      const response = await axios.get('http://127.0.0.1:3000/api/v2/messages/random');
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  },
+);
+
+const initialState = {
+  greetings: [],
+  isLoading: false,
+  error: null,
+};
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreetings.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchGreetings.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.greetings = action.payload;
+      })
+      .addCase(fetchGreetings.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default greetingsSlice.reducer;
